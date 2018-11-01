3c1d3ab1dcb831e9216c582f1d49f7c6b513d7a1
Yhspehy <670395851@qq.com>
2018-11-01 17:44:27 +0800

    add: profile message

--- eros-zbb/src/js/pages/profile/message.vue
+++ eros-zbb/src/js/pages/profile/message.vue
@@ -1,267 +1,115 @@
 <template>
-    <div class="setting-index">
-        <div class="setting-container item" @click="jump('myself')">
-            <text class="leftWord">个人信息</text>
-            <text class="icon">&#xf054;</text>
-        </div>
-
-        <div class="setting-container">
-            <div class="item" @click="jump('push')">
-                <text class="leftWord">推送通知</text>
-                <text class="icon">&#xf054;</text>
+    <div class="setting-push">
+        <div class="item" v-for="(item, idx) in list" :key="idx">
+            <div class="header-img">
+                <image class="avatar" src="http://temp.im/39x39" alt=""></image>
+                <div class="unread"></div>
             </div>
-            <div class="item">
-                <text class="leftWord">无图模式下(2G/3G/4G)</text>
-                <text class="icon">&#xf00c;</text>
-            </div>
-            <div class="item">
-                <text class="leftWord">WIFI下自动播放</text>
-                <text class="icon">&#xf00c;</text>
-            </div>
-            <div class="item" @click="clearCache">
-                <text class="leftWord">清除缓存</text>
-                <div class="flex-row-center">
-                    <text class="leftWord">39.9MB&nbsp;</text>
-                    <text class="icon">&#xf054;</text>
+            <div class="message">
+                <text class="title">{{item.title}}</text>
+                <text class="time">{{item.time}}</text>
+                <div class="content">
+                    <text class="text">{{item.text}}</text>
+                    <text class="detail-link">点击查看详情</text>
                 </div>
             </div>
         </div>
-
-        <div class="setting-container">
-            <div class="item" @click="jump('join')">
-                <text class="leftWord">加入我们</text>
-                <text class="icon">&#xf054;</text>
-            </div>
-            <div class="item" @click="jump('suggestion')">
-                <text class="leftWord">意见反馈</text>
-                <text class="icon">&#xf054;</text>
-            </div>
-        </div>
-
-        <div class="setting-container">
-            <div class="item" @click="showMask">
-                <text class="leftWord">推荐给好友</text>
-                <text class="icon">&#xf054;</text>
-            </div>
-            <div class="item">
-                <text class="leftWord">给我们评分鼓励下吧</text>
-                <text class="icon">&#xf054;</text>
-            </div>
-            <div class="item" @click="update">
-                <text class="leftWord">检测新版本</text>
-                <text class="icon">&#xf054;</text>
-            </div>
-            <div class="item" @click="jump('about')">
-                <text class="leftWord">关于我们</text>
-                <text>
-                    <text class="leftWord">1.0.0&nbsp;</text>
-                    <text class="icon">&#xf054;</text>
-                </text>
-            </div>
-        </div>
-
-        <text class="setting-container quit" @click="logout">退出当前账号</text>
-
-        <!-- 弹窗，分享 -->
-        <bmmask class="mask" animation="scale" position="center" :duration="400" ref="bmmask" :disableMaskEvent="true">
-            <bmpop class="modal-top">
-                <div class="share" ref='share'>
-                    <div style="align-items: center">
-                        <text class="shareHeader">分享到</text>
-                    </div>
-                    <div class="shareImgList">
-                        <div class="shareImgDiv">
-                            <image class="img" src="http://temp.im/60x60"></image>
-                            <text class="shareImgText">微信</text>
-                        </div>
-                        <div class="shareImgDiv">
-                            <image class="img" src="http://temp.im/60x60"></image>
-                            <text class="shareImgText">朋友圈</text>
-                        </div>
-                        <div class="shareImgDiv">
-                            <image class="img" src="http://temp.im/60x60"></image>
-                            <text class="shareImgText">QQ</text>
-                        </div>
-                        <div class="shareImgDiv">
-                            <image class="img" src="http://temp.im/60x60"></image>
-                            <text class="shareImgText">微博</text>
-                        </div>
-                    </div>
-                    <text class="closeMask" @click="closeMask">取消</text>
-                </div>
-            </bmpop>
-        </bmmask>
     </div>
 </template>
 
 <script>
-const domModule = weex.requireModule('dom');
-
 export default {
+    name: 'profile_setting_message',
     data() {
         return {
+            list: [
+                {
+                    avatar: '',
+                    title: '系统消息',
+                    time: '7月31日17:44',
+                    text:
+                        '这里是系统消息喂啊喂啊喂，这里是系统消息喂啊喂啊喂啊喂啊喂，点击查看更多就可以跳转到官方活动页面拉是不是很开心喂 >>'
+                },
+                {
+                    avatar: '',
+                    title: '系统消息',
+                    time: '7月31日17:44',
+                    text:
+                        '这里是系统消息喂啊喂啊喂，这里是系统消息喂啊喂啊喂啊喂啊喂，点击查看更多就可以跳转到官方活动页面拉是不是很开心喂 >>'
+                },
+                {
+                    avatar: '',
+                    title: '系统消息',
+                    time: '7月31日17:44',
+                    text:
+                        '这里是系统消息喂啊喂啊喂，这里是系统消息喂啊喂啊喂啊喂啊喂，点击查看更多就可以跳转到官方活动页面拉是不是很开心喂 >>'
+                }
+            ]
         };
-    },
-    beforeCreate() {
-        domModule.addRule('fontFace', {
-            fontFamily: 'fontAwesome',
-            src: "url('bmlocal://iconfont/fontawesome-webfont.ttf')"
-        });
-    },
-    methods: {
-        jump(name) {
-            this.$router.open({
-                name: `profile.${name}`
-            })
-        },
-        clearCache() {
-            this.$notice.confirm({
-                title: '清除缓存',
-                message: '是否清除缓存39.9MB',
-                okTitle: '确认',
-                cancelTitle: '取消',
-                okCallback() {
-                    console.log('yes');
-                },
-                cancelCallback() {
-                    console.log('no');
-                }
-            });
-        },
-        update() {
-            this.$notice.confirm({
-                title: '更新',
-                message: '当前版本:1.0.0,\n 是否更新至最新版本:2.2.2',
-                okTitle: '确认',
-                cancelTitle: '取消',
-                okCallback() {
-                    console.log('yes');
-                },
-                cancelCallback() {
-                    console.log('no');
-                }
-            });
-        },
-        logout() {
-            this.$notice.confirm({
-                title: '退出',
-                message: '是否退出当前账号',
-                okTitle: '确认',
-                cancelTitle: '取消',
-                okCallback() {
-                    console.log('yes');
-                },
-                cancelCallback() {
-                    console.log('no');
-                }
-            });
-        },
-        showMask() {
-            this.$refs['bmmask'].show();
-        },
-        closeMask() {
-            this.$refs['bmmask'].hide();
-        }
     }
 };
 </script>
 
-<style scoped>
-/* comment */
-.flex-row-center {
-    flex-direction: row;
-    align-items: center;
+<style scoped lang="scss">
+.setting-push {
+    background: #ffffff;
+    margin-top: 20px;
 }
-/* comment end */
-
-.icon {
-    font-family: fontAwesome;
-    color: #4d4d4d;
+.item {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    padding-left: 36px;
+    padding-right: 36px;
+    flex-direction: row;
+    width: 750px;
+}
+.header-img {
+    width: 78px;
+    height: 78px;
     margin-right: 20px;
 }
 
-.setting-container {
-    margin-top: 20px;
-    background-color: #ffffff;
+.avatar {
+    width: 78px;
+    height: 78px;
+    border-radius: 39px;
 }
-.item {
-    flex-direction: row;
-    justify-content: space-between;
-    padding-left: 36px;
-    padding-right: 20px;
-    height: 90px;
-    align-items: center;
-    border-bottom-style: solid;
-    border-bottom-color: #f2f2f2;
-    border-bottom-width: 1px;
-}
-
-.leftWord {
-    font-size: 28px;
-    color: #4d4d4d;
-}
-
-.quit {
-    font-size: 28px;
-    color: #f5303d;
-    text-align: center;
-    line-height: 90px;
-}
-
-.mask {
+.unread {
     position: absolute;
     top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.4);
+    right: 8px;
+    width: 14px;
+    height: 14px;
+    border-radius: 7px;
+    background-color: #ff512f;
 }
 
-
-.share {
-    width: 680px;
-    margin-left: 35px;
-    background-color: #eff0f3;
-    border-radius: 10px;
+.message {
+    color: #4d4d4d;
+    flex: 1;
 }
 
-.shareHeader {
-    margin-top: 30px;
-    margin-bottom: 30px;
-    margin-left: 40px;
-    margin-right: 40px;
-}
-
-.shareImgList {
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-}
-
-.shareImgDiv {
-    align-items: center;
-}
-
-.img {
-    width: 108px;
-    height: 108px;
-}
-
-.shareImgText {
+.title {
     font-size: 28px;
+    color: #808080;
+}
+.time {
     color: #b3b3b3;
-    margin-top: 10px;
-    margin-bottom: 10px;
+    font-size: 22px;
+    line-height: 50px;
+}
+.content {
+    flex-flow: row wrap;
+}
+.text {
+    color: #4d4d4d;
+    font-size: 24px;
+    line-height: 32px;
 }
 
-.closeMask {
-    width: 680px;
-    height: 90px;
-    background-color: #ffffff;
-    color: #808080;
-    line-height: 90px;
-    text-align: center;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
+.detail-link {
+    color: #0088ff;
+    font-size: 24px;
+    line-height: 32px;
 }
 </style>
